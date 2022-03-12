5bdd39b0dc04d5cd75f2b13fbe99d63b12af1394
Evgeniy Pavlovich Demyanov <evgeniy1demyanov@gmail.com>
2022-03-12 09:09:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+class StopWatch {
+  #started = false;
+  #startTime = null;
+  #duration = 0;
+  constructor() {
+    this.reset()
+  }
+  reset() {
+    this.#duration = 0
+    this.#started = false
+  }
+  get duration() {
+    return this.#duration;
+  }
+  start() {
+    if (this.#started) {
+      throw 'Запуск уже выполнен.'
+    }
+    this.#started = true
+    this.#startTime = Date.now();
+  }
+  stop() {
+    if (this.#started === false) {
+      throw 'Запуск не выполнялся'
+    }
+    this.#started = false
+    const endTime = Date.now();
+    this.#duration = (this.#startTime - endTime) / 1000
+  }
+}
+
+const sw = new StopWatch();
